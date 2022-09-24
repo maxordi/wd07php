faf3b84455bf08a4d8762059c001e5f9b9d13f8b
maxordi <max.ordashevskiy@yandex.by>
2022-09-24 21:52:57 +0300

    git test branch

--- public/index.php
+++ public/index.php
@@ -16,7 +16,8 @@
 //    echo "Год высокосный";
 //else
 //    echo "Год не высокосный";
-echo "git hello!";
+echo "Maxim";
+echo "Ordashevskiy";
 //$array = [10, 22, 11];
 //echo (2);
 //     echo (1);
