871df07666fb504063aac42e273e09461f6d7898
maxordi <max.ordashevskiy@yandex.by>
2022-09-24 21:54:52 +0300

    my homework

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@
 //    echo "Год высокосный";
 //else
 //    echo "Год не высокосный";
-echo "git second branch";
+echo "Maksim Ordashevskiy";
 //$array = [10, 22, 11];
 //echo (2);
 //     echo (1);
